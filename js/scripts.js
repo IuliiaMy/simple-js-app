689066aa73bb87195d899c284c197db8f4e7271f
IuliiaMy <illiassayulia@gmail.com>
2023-07-22 22:27:42 +0200

    Added Pokemon Objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello World')
+let pokemonList = [];
 
-let favouriteFood = 'Borsch';
-document.write(favouriteFood);
+pokemonList[0] = {
+    name : "Persian",
+    height : 1,
+    types : ['dark', 'rock', 'ghost']
+};
+
+pokemonList[1] = {
+    name : "Pikachu",
+    height : 0.4,
+    types : ['electric', 'fighting', 'poison']
+};
+
+pokemonList[2] = {
+    name : "Arbok",
+    height : 3.5,
+    types : ['dark', 'grass', 'poison']
+};
+
+console.log(pokemonList);
