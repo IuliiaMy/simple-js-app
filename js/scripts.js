a2b9254e313d6559b1707146596819879cc0ab8c
IuliiaMy <illiassayulia@gmail.com>
2023-07-25 17:10:57 +0200

    Added for loop and condition to show a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
-let pokemonList = [];
-
-pokemonList[0] = {
+let pokemonList = [{
     name : "Persian",
     height : 1,
     types : ['dark', 'rock', 'ghost']
-};
-
-pokemonList[1] = {
+},
+{
     name : "Pikachu",
     height : 0.4,
     types : ['electric', 'fighting', 'poison']
-};
-
-pokemonList[2] = {
+},
+{
     name : "Arbok",
     height : 3.5,
     types : ['dark', 'grass', 'poison']
-};
+}];
 
 console.log(pokemonList);
+
+for(let i=0; i<pokemonList.length; i++){//code iterates through pokemon array
+    let result = pokemonList[i].height >1.0 ? 'Wow that\'s big' : '' // a condition in the for loop that returns a comment as soon as height is bigger than 1.0, othervise returns an empmty string
+    document.write(`${pokemonList[i].name} (height:${pokemonList[i].height}) ${result}<br>`)//prints a pokemon name as well as the height and a comment 
+}
