e9745befa7d21efad0257c3f0218ba6a240c492f
IuliiaMy <illiassayulia@gmail.com>
2023-07-25 17:26:22 +0200

    Added CSS styling to the text

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,5 @@ console.log(pokemonList);
 
 for(let i=0; i<pokemonList.length; i++){//code iterates through pokemon array
     let result = pokemonList[i].height >1.0 ? 'Wow that\'s big' : '' // a condition in the for loop that returns a comment as soon as height is bigger than 1.0, othervise returns an empmty string
-    document.write(`${pokemonList[i].name} (height:${pokemonList[i].height}) ${result}<br>`)//prints a pokemon name as well as the height and a comment 
+    document.write(`<div class=pokemonNameAndHeight>${pokemonList[i].name} (height:${pokemonList[i].height}) ${result}</div><br>`)//prints a pokemon name as well as the height and a comment 
 }
