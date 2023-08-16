f9cc696d746eeb0f7adcac9e9f875aadb1214112
IuliiaMy <illiassayulia@gmail.com>
2023-08-16 21:17:03 +0200

    Implemented IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
-let pokemonList = [{
-    name : "Persian",
-    height : 1,
-    types : ['dark', 'rock', 'ghost']
-},
-{
-    name : "Pikachu",
-    height : 0.4,
-    types : ['electric', 'fighting', 'poison']
-},
-{
-    name : "Arbok",
-    height : 3.5,
-    types : ['dark', 'grass', 'poison']
-}];
+let pokemonRepository = (function(){
+    let pokemonList = [{
+        name : "Persian",
+        height : 1,
+        types : ['dark', 'rock', 'ghost']
+    },
+    {
+        name : "Pikachu",
+        height : 0.4,
+        types : ['electric', 'fighting', 'poison']
+    },
+    {
+        name : "Arbok",
+        height : 3.5,
+        types : ['dark', 'grass', 'poison']
+    }];
 
-console.log(pokemonList);
+    function add(pokemon){
+        pokemonList.push(pokemon)
+    }
 
-pokemonList.forEach(function(pokemon){
+    function getAll(){
+        return pokemonList
+    }
+
+    return{
+        add: add,
+        getAll: getAll
+    };
+
+
+})();
+
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon){
     let result = pokemon.height >1.0 ? 'Wow that\'s big' : '' 
     document.write(`<div class=pokemonNameAndHeight>${pokemon.name} (height:${pokemon.height}) ${result}</div><br>`)
 });
